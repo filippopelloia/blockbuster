319e5848ef11e83dea04c6c8b095a0c747b4cf60
Dev1 - filip <pelloiafilippo@live.com>
2026-02-13 09:49:35 +0100

    Added standard choices

--- app.py
+++ app.py
@@ -1,8 +1,49 @@
 # Blockbuster catalog
 
-main_menu = {1: 'Add a movie to the catalog',
-             2: 'Show catalog',
-             3: 'Seach',
-             4: 'Remove a movie from the catalog',
-             5: 'Analytics',
-             6: 'Exit'}
+def main():
+
+    main_menu = {1: 'Add a movie to the catalog',
+                 2: 'Show catalog',
+                 3: 'Seach',
+                 4: 'Remove a movie from the catalog',
+                 5: 'Analytics',
+                 6: 'Exit'}
+
+    sub_menu = {}
+
+    is_running = True
+
+    while is_running:
+
+        for key, value in main_menu.items():
+            print(f"{key} - {value}")
+
+        choice = input("What do you want to do? Choose (1-6): ")
+
+        if choice.isdigit():
+            choice = int(choice)
+
+            if choice in main_menu.keys():
+                if choice == 1:
+                    pass
+                elif choice == 2:
+                    pass
+                elif choice == 3:
+                    pass
+                elif choice == 4:
+                    pass
+                elif choice == 5:
+                    pass
+                elif choice == 6:
+                    print("Have a nice day!")
+                    is_running = False
+                else:
+                    print("Invalid value")
+            else:
+                print("Invalid value")
+        else:
+            print("Invalid value")
+
+
+if __name__ == '__main__':
+    main()
