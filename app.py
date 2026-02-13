086e62c626c79d7ce4b3b35f85ee15da6693a61d
Dev1 - filip <pelloiafilippo@live.com>
2026-02-13 17:16:00 +0100

    updated analytics area

--- app.py
+++ app.py
@@ -21,10 +21,10 @@ def ask_rating():
 def add_movie():
     print("Add a new movie to the catalog")
 
-    title_movie = input("Insert the title: ")
+    title_movie = input("Insert the title: ").capitalize()
     year_movie = input("Inser the year: ")
-    genre = input("Inser the genre: ")
-    director = input("Inser the director: ")
+    genre = input("Inser the genre: ").capitalize()
+    director = input("Inser the director: ").capitalize()
     duration = input("Inser the duration (minutes): ")
     rating = ask_rating()
 
@@ -64,27 +64,145 @@ def show_catalog(movie_catalog):
 
 
 def search_movie():
-    pass
+
+    while True:
+        search = input("Search for a movie by title: ").lower()
+
+        print(f"Result value: {search}")
+        print(movie_catalog.movies)
+
+        if search:
+            print("----------------------------------------------------------------------------------------------------------------------------------------------------")
+            print("Result:")
+            print("----------------------------------------------------------------------------------------------------------------------------------------------------")
+            for movie in movie_catalog.movies:
+                if search in movie.title_movie.lower():
+                    print(movie)
+            print("----------------------------------------------------------------------------------------------------------------------------------------------------")
+        else:
+
+            while True:
+                for movie in movie_catalog.movies:
+                    print(movie)
+
+                action = input("'q' to quit: ")
+
+                if action == 'q':
+                    print("Have a nice day")
+                    break
+                else:
+                    continue
+
+        break
 
 
 def remove_movie():
-    pass
+
+    print("----------------------------------------------------------------------------------------------------------------------------------------------------")
+    print("Remove a movie from the catalog")
+    print("----------------------------------------------------------------------------------------------------------------------------------------------------")
+
+    for movie in movie_catalog.movies:
+        print(movie)
+    print("----------------------------------------------------------------------------------------------------------------------------------------------------")
+
+    while True:
+        exit_from_catalot = input(
+            "Insert the index number to remove a movie. Type 'q' to quit: ")
+        print("----------------------------------------------------------------------------------------------------------------------------------------------------")
+
+        if exit_from_catalot.lower() == 'q':
+            break
+        elif exit_from_catalot.isdigit():
+            num_chosen = int(exit_from_catalot) - 1
+            if 0 > num_chosen or num_chosen > len(movie_catalog.movies):
+                print("Invalid number")
+            else:
+
+                while True:
+                    second_question = input(
+                        f"Do you want to remove '{movie_catalog.movies[num_chosen].title_movie}' from catalog? (Y/N): ").lower()
+
+                    if second_question == 'y':
+                        del movie_catalog.movies[num_chosen]
+                        print("----------------------------------------------------------------------------------------------------------------------------------------------------")
+                        print("Successfully deleted!")
+                        print("----------------------------------------------------------------------------------------------------------------------------------------------------")
+                        print("Catalog updated:")
+                        for movie in movie_catalog.movies:
+                            print(movie)
+                        break
+                    else:
+                        print("Nothing happened")
+                        break
+        else:
+            print("Invalid value")
 
 
-def show_analytics():
-    pass
+def show_analytics(analytics_menu):
+    # media voti, classifica voti, voto più basso, voto più alto,
+    # media durata, classifica durata, più breve, più lungo
+    # più vecchio. più recente, film per anno
+    # raccolta film per regista
+    # film per genere
+    # ordine alfabetico titoli film
+
+    for key, value in analytics_menu.items():
+        print(f"{key} - {value}")
+
+    while True:
+        analytics_action = input(
+            "What do you want to do? Choose a number (1-7): ")
+
+        if analytics_action.isdigit():
+            analytics_action = int(analytics_action)
+
+            if 1 >= analytics_action and analytics_action >= 7:
+                print("Invalid value, choose a number (1-7): ")
+            else:
+                if analytics_action == 1:
+                    pass
+                elif analytics_action == 2:
+                    pass
+                elif analytics_action == 3:
+                    pass
+                elif analytics_action == 4:
+                    pass
+                elif analytics_action == 5:
+                    pass
+                elif analytics_action == 6:
+                    pass
+                elif analytics_action == 7:
+                    print("Have a nice day!")
+                    break
+                else:
+                    print("Something went wrong")
+        else:
+            print("Invalid value, choose a number (1-7): ")
 
 
 def main():
 
     main_menu = {1: 'Add a movie to the catalog',
                  2: 'Show catalog',
-                 3: 'Seach',
+                 3: 'Seach by title',
                  4: 'Remove a movie from the catalog',
                  5: 'Analytics',
                  6: 'Exit'}
 
-    sub_menu = {}
+    analytics_menu = {1: 'Ratings',
+                      2: 'Duration',
+                      3: 'Years',
+                      4: 'Directors',
+                      5: 'Genres',
+                      6: 'Titles',
+                      7: 'Exit'}
+
+    rating_menu = {1: 'Average rating',
+                   2: 'Highest rated movie',
+                   3: 'Lowest rated movie',
+                   4: 'Rating ranking',
+                   5: 'Exit'}
 
     is_running = True
 
@@ -108,7 +226,11 @@ def main():
                 elif choice == 4:
                     remove_movie()
                 elif choice == 5:
-                    show_analytics()
+                    # show_analytics(analytics_menu)
+                    print("----------------------------------------------------------------------------------------------------------------------------------------------------")
+                    print("Work in progress, stay tuned!")
+                    print("----------------------------------------------------------------------------------------------------------------------------------------------------")
+                    pass
                 elif choice == 6:
                     print("Have a nice day!")
                     is_running = False
