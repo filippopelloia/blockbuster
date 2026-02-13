2f24e91148563e23def5004e1f0af81edbb56117
Dev1 - filip <pelloiafilippo@live.com>
2026-02-13 09:30:09 +0100

    Added text for main menu

---
+++ app.py
@@ -0,0 +1,8 @@
+# Blockbuster catalog
+
+main_menu = {1: 'Add a movie to the catalog',
+             2: 'Show catalog',
+             3: 'Seach',
+             4: 'Remove a movie from the catalog',
+             5: 'Analytics',
+             6: 'Exit'}
